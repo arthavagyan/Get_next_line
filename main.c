7a42147c5c61defa5731350741d20f948e710842
Artur Avagyan <artavagy@c2r14s4.42yerevan.am>
2026-03-16 19:26:31 +0400

    sda

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
-#include "get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artavagy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 18:31:33 by artavagy          #+#    #+#             */
+/*   Updated: 2026/03/14 19:26:08 by artavagy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//#include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -6,11 +17,11 @@
 int	main(void)
 {
 	ssize_t	count;
-	char	buf[6];
+	char	buf[7];
 	int	fd;
 
 	fd = open("test.txt", O_RDONLY);
-	count = read(fd, buf, 5);
+	count = read(fd, buf, 6);
 	buf[count] = '\0';
 	printf("%s", buf);
 	close(fd);
